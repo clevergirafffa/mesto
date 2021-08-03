255331286ce6a908ac2b7ff54e41f9ba47692e82
Anna Pedakhovskaya <clever.girafffa@gmail.com>
2021-08-03 16:35:55 +0200

    fix all critical issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
 const closeButton = document.querySelector('.popup__close');
 const editButton = document.querySelector('.profile__editbutton');
 const popup = document.querySelector('.popup');
-const profileNameField = document.querySelector('.profile__namedata');
+const profileNameField = document.querySelector('.profile__name');
 const profileOccupationField = document.querySelector('.profile__occupation');
 const popupNameField = document.querySelector('.popup__name');
 const popupOccupationField = document.querySelector('.popup__occupation');
 const form = document.getElementById('profile-edit');
 
-popupNameField.setAttribute('value', profileNameField.textContent)
-popupOccupationField.setAttribute('value', profileOccupationField.textContent)
+
+function copyInitialValuesAndOpenPopup(){
+    popupNameField.setAttribute('value', profileNameField.textContent);
+    console.log(profileNameField.textContent);
+    popupOccupationField.setAttribute('value', profileOccupationField.textContent);
+    console.log(profileOccupationField.textContent);
+    togglePopup();
+}
+
 
 function togglePopup() {
     popup.classList.toggle('popup_is-opened');
@@ -16,12 +23,14 @@ function togglePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    profileNameField.textContent = form.elements[0].value;
-    profileOccupationField.textContent = form.elements[1].value;
+    let name = document.querySelector('#name');
+    let occupation = document.querySelector('#occupation');
+    profileNameField.textContent = name.value;
+    profileOccupationField.textContent = occupation.value;
     togglePopup();
 }
 
-editButton.addEventListener("click", togglePopup)
+editButton.addEventListener("click", copyInitialValuesAndOpenPopup)
 closeButton.addEventListener("click", togglePopup)
 form.addEventListener('submit', formSubmitHandler);
 

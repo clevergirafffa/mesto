e6845b722ee860ecbaafb8a9dee6cfad7a7b2504
Anna Pedakhovskaya <clever.girafffa@gmail.com>
2021-08-04 13:18:56 +0200

    fix all critical issues and part of non-critical

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,18 @@ const editButton = document.querySelector('.profile__editbutton');
 const popup = document.querySelector('.popup');
 const profileNameField = document.querySelector('.profile__name');
 const profileOccupationField = document.querySelector('.profile__occupation');
-const popupNameField = document.querySelector('.popup__name');
-const popupOccupationField = document.querySelector('.popup__occupation');
+const popupNameField = document.querySelector('.popup__input_type_name');
+const popupOccupationField = document.querySelector('.popup__input_type_occupation');
 const form = document.getElementById('profile-edit');
+let name = document.querySelector('#name');
+let occupation = document.querySelector('#occupation');
 
 
 function copyInitialValuesAndOpenPopup(){
     popupNameField.setAttribute('value', profileNameField.textContent);
-    console.log(profileNameField.textContent);
     popupOccupationField.setAttribute('value', profileOccupationField.textContent);
-    console.log(profileOccupationField.textContent);
     togglePopup();
 }
-
 
 function togglePopup() {
     popup.classList.toggle('popup_is-opened');
@@ -23,8 +22,6 @@ function togglePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    let name = document.querySelector('#name');
-    let occupation = document.querySelector('#occupation');
     profileNameField.textContent = name.value;
     profileOccupationField.textContent = occupation.value;
     togglePopup();
